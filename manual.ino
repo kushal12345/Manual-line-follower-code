a679de587211801bf17b19f94c8948ceb2c05d38
Kushal Luitel <kushal12345@users.noreply.github.com>
2018-01-07 12:47:50 +0545

    Add files via upload

---
+++ manual.ino
@@ -0,0 +1,47 @@
+ char c=0;
+void setup () {
+  pinMode(2,OUTPUT);// left motor pin 1
+  pinMode(3,OUTPUT);// left motor pin2
+  pinMode(4,OUTPUT);// right motor pin 1 
+  pinMode(5,OUTPUT);// right motor  pin 2
+  Serial.begin(9600);//enable serial communication
+}
+void loop () { 
+  if (Serial.available()){
+  c=Serial.read();
+ if(c=='w') forward();
+ if(c=='a') left();
+ if(c=='s') right();
+ if(c=='d') Stop();
+  }
+}
+
+void forward() {
+  digitalWrite(2,HIGH);
+  digitalWrite(3,LOW);
+  digitalWrite(7,HIGH);
+  digitalWrite(8,LOW);
+} 
+
+void left() 
+{
+  digitalWrite(2,LOW);
+  digitalWrite(3,LOW);
+  digitalWrite(7,HIGH); 
+  digitalWrite(8,LOW); 
+} 
+
+ 
+void Stop() {
+  digitalWrite(2,LOW);
+  digitalWrite(3,LOW);
+  digitalWrite(7,LOW);
+  digitalWrite(8,LOW);
+} 
+void right() 
+{
+  digitalWrite(2,HIGH);
+  digitalWrite(3,LOW);
+  digitalWrite(7,LOW); 
+  digitalWrite(8,LOW); 
+}
